7d6ef88b8c2aa9ef3241d8f7441bc2851d798c8d
Melvin14112004 <melvinsolomon200@gmail.com>
2025-07-28 23:40:07 +0530

    Fix: updated tokenizer creation to avoid pickle loading error

--- save_tokenizer.py
+++ save_tokenizer.py
@@ -1,5 +1,5 @@
+from keras.preprocessing.text import Tokenizer
 import pandas as pd
-from tensorflow.keras.preprocessing.text import Tokenizer
 import pickle
 
 df = pd.read_csv("dataset/IMDB Dataset.csv")
@@ -7,4 +7,4 @@ tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
 tokenizer.fit_on_texts(df['review'])
 
 with open("tokenizer.pkl", "wb") as f:
-    pickle.dump(tokenizer, f, protocol=4)
+    pickle.dump(tokenizer, f)
